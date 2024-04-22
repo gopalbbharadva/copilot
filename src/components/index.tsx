a252e466efc96a8c912af884583bb37e2d9392fb
gopalbbharadva <gopalbbharadva@gmail.com>
2024-04-22 20:18:50 +0530

    Feat: barrel export for the components added

---
+++ src/components/index.tsx
@@ -0,0 +1,21 @@
+import { FirstQuestion } from './FirstQuestion'
+import { MotionWrapper } from './MotionWrapper'
+import { Question } from './Question'
+import { QuestionWithAvatar } from './QuestionWithAvatar'
+import { SecondQuestion } from './SecondQuestion'
+import { SubHeader } from './SubHeader'
+import { ThirdQuestion } from './ThirdQuestion'
+import { Answer } from './static/Answer'
+import { CodeSnippet } from './static/CodeSnippet'
+
+export {
+  Question,
+  QuestionWithAvatar,
+  MotionWrapper,
+  FirstQuestion,
+  SecondQuestion,
+  ThirdQuestion,
+  SubHeader,
+  Answer,
+  CodeSnippet,
+}
